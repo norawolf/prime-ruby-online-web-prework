710e7dadb96853232a6c09793f86b51254494656
Nora Vogt <noraevogt@gmail.com>
2018-11-24 01:00:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,15 @@
 # Add  code here!
 def prime?(n)
-  if n <= 1
-        false
-    else
-        (2..(n-1)).none? { |i| n % i == 0}
-    end
+
+if n <= 1
+  false
+end
+ (2..(n-1)).any? { |d| n % d == 0 } ? false : true
+
+
+  #if n <= 1
+  #      false
+  #  else
+  #      (2..(n-1)).none? { |i| n % i == 0}
+    #end
 end
